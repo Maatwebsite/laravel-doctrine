9ef105846c751d7dd6a2197a4f861d0a09423b7f
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-03-02 20:42:43 +0100

    Try without facade

--- src/Cache/RedisProvider.php
+++ src/Cache/RedisProvider.php
@@ -1,13 +1,12 @@
 <?php namespace Mitch\LaravelDoctrine\Cache;
 
 use Doctrine\Common\Cache\RedisCache;
-use Redis;
 
 class RedisProvider implements Provider 
 {
     public function make($config = null) 
     {
-        $redis = new Redis;
+        $redis = app('redis');
         $redis->connect($config['host'], $config['port']);
         if (isset($config['database']))
             $redis->select($config['database']);
