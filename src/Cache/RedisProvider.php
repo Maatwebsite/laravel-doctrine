f139a979fe70d61491744d06cb0a6105f37b0c77
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2015-03-02 20:45:03 +0100

    Update RedisProvider.php

--- src/Cache/RedisProvider.php
+++ src/Cache/RedisProvider.php
@@ -1,12 +1,13 @@
 <?php namespace Mitch\LaravelDoctrine\Cache;
 
+use Redis;
 use Doctrine\Common\Cache\RedisCache;
 
 class RedisProvider implements Provider 
 {
     public function make($config = null) 
     {
-        $redis = app('redis');
+        $redis = new Redis;
         $redis->connect($config['host'], $config['port']);
         if (isset($config['database']))
             $redis->select($config['database']);
